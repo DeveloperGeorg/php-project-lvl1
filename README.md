14075ec696216efa217b71d220fe0940549b0ec1
developergeorg <bg@moderntoken.com>
2020-06-06 11:53:01 +0300

    [brain-progression] Progression game was added

--- README.md
+++ README.md
@@ -16,3 +16,5 @@ Hexlet. Brain games.
 #### Gcd game
 [![Play process video](https://i.paste.pics/afbc397f92c066636fecf5cd4a71edc0.png?trs=9525980e4087a3ec14e8632d0e4ba14812bb664b488e3d231347042fab02a0e7)](https://asciinema.org/a/ZIAIk1BnBZf16NH2bebYVhhRR)
 
+#### Progression game
+[![Play process video](https://i.paste.pics/afbc397f92c066636fecf5cd4a71edc0.png?trs=9525980e4087a3ec14e8632d0e4ba14812bb664b488e3d231347042fab02a0e7)](https://asciinema.org/a/55JVfz0GsZoRVDhZTRcZxlYdD)
---
+++ src/ProgressionGame.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace BrainGames;
+
+use BrainGames\Model\Task;
+
+/**
+ * Class ProgressionGame
+ *
+ * @package BrainGames
+ */
+class ProgressionGame extends GameAbstract
+{
+    /**
+     * @var int
+     */
+    private $progressionLength = 10;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDescription(): string
+    {
+        return 'What number is missing in the progression?';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTasks(int $count): array
+    {
+        $tasks = [];
+        $taskCounter = 0;
+        while ($taskCounter < $count) {
+            $startNumber = rand(1, 999);
+            $progressionDifference = rand(1, 100);
+            $progression = $this->generateProgression(
+                $startNumber,
+                $progressionDifference,
+                $this->getProgressionLength()
+            );
+            $hiddenPosition = rand(1, count($progression)) - 1;
+            $answer = $progression[$hiddenPosition];
+            $progression[$hiddenPosition] = '..';
+            $tasks[] = new Task(
+                implode(' ', $progression),
+                $answer
+            );
+            $taskCounter++;
+        }
+
+        return $tasks;
+    }
+
+    /**
+     * @param int $start
+     * @param int $progressionDifference
+     * @param int $length
+     *
+     * @return array
+     */
+    private function generateProgression(int $start, int $progressionDifference, int $length): array
+    {
+        $progression = [];
+        $number = $start;
+        $progression[] = $number;
+        while (count($progression) < $length) {
+            $number += $progressionDifference;
+            $progression[] = $number;
+        }
+        return $progression;
+    }
+
+    /**
+     * @return int
+     */
+    public function getProgressionLength(): int
+    {
+        return $this->progressionLength;
+    }
+
+    /**
+     * @param int $progressionLength
+     *
+     * @return ProgressionGame
+     */
+    public function setProgressionLength(int $progressionLength): ProgressionGame
+    {
+        $this->progressionLength = $progressionLength;
+        return $this;
+    }
+
+}
