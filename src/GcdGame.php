940bd3e144811aff57eae3af92ea0e8103e5b6e9
developergeorg <bg@moderntoken.com>
2020-06-06 11:11:27 +0300

    [gcd-game] gcd game was added

---
+++ src/GcdGame.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace BrainGames;
+
+use BrainGames\Model\Task;
+use Exception;
+
+use function gmp_gcd;
+
+/**
+ * Class GcdGame
+ *
+ * @package BrainGames
+ */
+class GcdGame extends GameAbstract
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDescription(): string
+    {
+        return 'Find the greatest common divisor of given numbers.';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTasks(int $count): array
+    {
+        $return = [];
+        $taskCounter = 0;
+        while ($taskCounter < $count) {
+            try {
+                $number1 = random_int(1, 999);
+                $number2 = random_int(1, 999);
+            } catch (Exception $e) {
+                $number1 = rand(1, 999);
+                $number2 = rand(1, 999);
+            }
+            $return[] = new Task(
+                "{$number1} {$number2}",
+                gmp_gcd($number1, $number2)
+            );
+            $taskCounter++;
+        }
+
+        return $return;
+    }
+}
